ec5ab53b6bcda8a8168966d2fdff2c0626b5c846
reecedantin <reecedantin@yahoo.com>
2014-12-08 17:16:22 -0500

    added first text

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -62,7 +62,15 @@ HTMLActuator.prototype.addTile = function (tile) {
   this.applyClasses(wrapper, classes);
 
   inner.classList.add("tile-inner");
-  inner.textContent = tile.value;
+  
+  if (tile.value = 2)
+  {
+    inner.textContent = "shit";
+  }
+  else
+  {
+    inner.textContent = tile.value;
+  }
 
   if (tile.previousPosition) {
     // Make sure that the tile gets rendered in the previous position first
