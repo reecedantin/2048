bf88e0ac115e93a6f8b062464a60b4ed96d09ad0
reecedantin <reecedantin@yahoo.com>
2014-12-08 17:09:45 -0500

    Change Keyboard again

--- js/keyboard_input_manager.js
+++ js/keyboard_input_manager.js
@@ -122,7 +122,7 @@ KeyboardInputManager.prototype.listen = function () {
 
     if (Math.max(absDx, absDy) > 10) {
       // (right : left) : (down : up)
-      self.emit("move", absDx > absDy ? (dx > 0 ? 1 : 3) : (dy > 0 ? 2 : 0));
+      self.emit("move", absDx > absDy ? (dx > 0 ? 3 : 1) : (dy > 0 ? 0 : 2));
     }
   });
 };
